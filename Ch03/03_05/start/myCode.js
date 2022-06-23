25e6625bc199dd263ee7f632a93c87d4fcada117
iareful <oonol.all@gmail.com>
2022-06-23 14:58:44 +0600

    init 03_05

---
+++ Ch03/03_05/start/myCode.js
@@ -0,0 +1,26 @@
+const events = require("events");
+
+let emitter = new events.EventEmitter();
+
+emitter.on("customEvent", (message, user) => {
+  console.log(`${user}: ${message}`);
+});
+
+emitter.emit("customEvent", "Hello World", "Computer");
+emitter.emit("customEvent", "That's pretty cool", "Eve");
+emitter.emit("customEvent", "That's pretty cool", "Eva");
+
+process.stdin.on("data", (data) => {
+  const input = data.toString().trim();
+  if (input === "exit") {
+    emitter.emit("customEvent", "Goodbye!", "process");
+    process.exit();
+  }
+  emitter.emit(
+    "customEvent",
+    data.toString().trim(),
+    "terminal"
+  );
+});
+// deep understanding + practice Needed 
+// add some use case
